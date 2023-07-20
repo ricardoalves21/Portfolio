581044f50e26a598cc5007868d271d721e635d15
Ricardo Alves <dev.rical21@gmail.com>
2023-07-19 22:39:57 -0300

    Inicio da criação do menu principal

---
+++ portfolio/src/app/portfolio/portfolio.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PortfolioService } from './services/portfolio.service';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  providers: [PortfolioService],
+})
+export class PortfolioModule {}
